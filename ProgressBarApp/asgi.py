64ce141e4d85cd4e3d00d95e18d947cfaab0b7ba
Muhammad Hamza <hamzaashraf@systemheuristics.com>
2024-02-10 15:12:49 +0500

    extra code files removed

--- ProgressBarApp/asgi.py
+++ ProgressBarApp/asgi.py
@@ -9,18 +9,5 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import progressbar.routing  # Import your routing configuration
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProgressBarApp.settings')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            progressbar.routing.websocket_urlpatterns
-        )
-    ),
-})
--- progressbar/routing.py
+++
@@ -1,9 +0,0 @@
-# yourapp/routing.py
-
-from django.urls import path
-from .consumers import ProgressConsumer
-
-websocket_urlpatterns = [
-    path('ws/progress/', ProgressConsumer.as_asgi()),
-    # Add more WebSocket URL patterns as needed
-]
